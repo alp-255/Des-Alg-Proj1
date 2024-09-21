249f111519c39cda3468aa7fc3923d828b8f98e4
Sergio Fersan <sergio.fersan.sss@gmail.com>
2024-09-21 20:06:38 -0300

    Criei o menu principal

---
+++ projeto.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void menu(){
+    printf("Menu:\n");
+    printf("1. Consultar Saldo\n"
+    "2. Consultar Extrato\n"
+    "3. Depositar Reais\n"
+    "4. Sacar Reais\n"
+    "5. Comprar Criptomoedas\n"
+    "6. Vender Criptomoedas\n"
+    "7. Atualizar Cotacao\n"
+    "8. Sair");
+}
+
+int main(){
+    menu();
+
+    return 0;
+}
