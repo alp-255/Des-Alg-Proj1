a86c87bff59ecab6f7159f84ed4873466974dfc2
Sergio Fersan <sergio.fersan.sss@gmail.com>
2024-09-23 17:04:15 -0300

    Testes funcionando no arquivo

--- projeto.c
+++ projeto.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void menu(){
     printf("Menu:\n");
@@ -9,11 +10,35 @@ void menu(){
     "5. Comprar Criptomoedas\n"
     "6. Vender Criptomoedas\n"
     "7. Atualizar Cotacao\n"
-    "8. Sair");
+    "8. Sair\n");
+}
+
+void escArq(char arquivo[]){
+    char tmp[10];
+    strcpy(tmp, arquivo);
+
+    strcat(tmp, "teste.txt");
+
+    FILE *arq = fopen(tmp, "a");
+
+    if(arq == NULL){
+        printf("erro\n");
+    }
+    fprintf(arq, "teste\n");
+    fclose(arq);
 }
 
 int main(){
     menu();
+    char esc[3];
+    char arquivo[6] = "user";
 
+    printf("Digite 1 para user 1 e 2 para user 2: \n");
+    scanf("%s", esc);
+
+    strcat(arquivo, esc);
+    strcat(arquivo, "/");
+
+    escArq(arquivo);
     return 0;
 }
