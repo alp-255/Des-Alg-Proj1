cd8a2902dd43d64173ecba7862210db79edb7a4d
Sergio Fersan <sergio.fersan.sss@gmail.com>
2024-09-26 16:16:38 -0300

    criei o escVar

--- projeto.c
+++ projeto.c
@@ -37,7 +37,7 @@ void escArq(char arquivo[]){
 
     // // escArq(strcat(strcat(arquivo, esc), "/"));  Adiciona a escolha e "/" ao final de "user"
 
-float lerArquivo(const char *variavel, const char *arquivo){
+float lerVar(const char *variavel, const char *arquivo){
     FILE *arq = fopen(arquivo, "r");
 
     char linha[100];
@@ -56,8 +56,45 @@ float lerArquivo(const char *variavel, const char *arquivo){
     return 0;
 }
 
+void escVar(const char *arquivo, const char *variavel, int valor){
+    FILE *arq = fopen(arquivo, "r");
+    
+    int achouVar = 0; // diz se achou a variavel no txt
+    char linha[100];
+    char buffer[100];
+
+    while(fgets(linha, sizeof(linha), arq)){
+        char tmpVar[15];
+        int tmpVal;
+        
+        if(sscanf(linha, "%[^ ] %d", tmpVar, &tmpVal) == 2){
+            if(strcmp(tmpVar, variavel) != 0){ // se as variaveis forem dif
+                snprintf(buffer + strlen(buffer), sizeof(buffer) - strlen(buffer), "%s %d\n", tmpVar, tmpVal);
+            } else{
+                snprintf(buffer + strlen(buffer), sizeof(buffer) - strlen(buffer), "%s %d\n", tmpVar, valor);
+                achouVar = 1;
+            }
+        } else{
+            strncat(buffer, linha, sizeof(buffer) - strlen(buffer) - 1); // deixa a linha igual se nn for usar
+        }
+    } // nada disso ta escrevendo ainda, so armazenando em variavel
+    fclose(arq);
+
+    if(achouVar == 1){
+        arq = fopen(arquivo, "w");
+        fputs(buffer, arq);
+        fclose(arq);
+    }
+}
+
 int main(){
-    printf("%f", lerArquivo("var2", "user1/var.txt"));
+    printf("%f\n", lerVar("var2", "user1/var.txt"));
+    escVar("user1/var.txt", "var2", 300);
+    printf("%f\n", lerVar("var2", "user1/var.txt"));
+    escVar("user1/var.txt", "var2", 9);
+    printf("%f\n", lerVar("var2", "user1/var.txt"));
+    escVar("user1/var.txt", "var1", 666);
+    printf("%f\n", lerVar("var1", "user1/var.txt"));
 
     return 0;
 }
